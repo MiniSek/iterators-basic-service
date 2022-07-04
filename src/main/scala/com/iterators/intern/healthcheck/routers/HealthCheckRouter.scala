5c1d02de34f4d22bc9596005533afe7340ada158
dominik <dominiksulik20@gmail.com>
2022-07-04 09:05:54 +0200

    handle information about db latency

--- src/main/scala/com/iterators/intern/healthcheck/routers/HealthCheckRouter.scala
+++ src/main/scala/com/iterators/intern/healthcheck/routers/HealthCheckRouter.scala
@@ -1,12 +1,19 @@
 package com.iterators.intern.healthcheck.routers
 
-import akka.http.scaladsl.server.Directives.{complete, get, path}
+import akka.http.scaladsl.server.Directives.{complete, get, onComplete, path}
 import akka.http.scaladsl.server.Route
+import com.iterators.intern.healthcheck.services.{HealthCheckService, HealthCheckServiceImpl}
+
+import scala.util.{Failure, Success}
+
 
 trait HealthCheckRouter {
-  val routes: Route = path("healthcheck") {
-    get {
-      complete("Healthcheck response")
+  val healthCheckService: HealthCheckService = new HealthCheckServiceImpl
+
+  val routes: Route = (get & path("healthcheck")) {
+    onComplete(healthCheckService.getDBLatency) {
+      case Success(latency) => complete(s"Latency is $latency microseconds")
+      case Failure(_)    => complete(s"Database is unreachable")
     }
   }
 }
